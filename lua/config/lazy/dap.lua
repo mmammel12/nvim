84abc7ef845067ad8d0dbfda9e3e7acf0eb626f6
Marty Mammel <mmammel12@gmail.com>
2024-03-12 08:55:40 -0700

    fix dap

--- lua/config/lazy/dap.lua
+++ lua/config/lazy/dap.lua
@@ -27,7 +27,7 @@ return {
 		"mxsdev/nvim-dap-vscode-js",
 		dependencies = { "mfussenegger/nvim-dap" },
 		config = function()
-			require("dap-vscode").setup({
+			require("dap-vscode-js").setup({
 				adapters = { "pwa-node", "pwa-chrome" },
 			})
 
