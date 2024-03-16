6dcb6aaedc912103658b3575a840ec2e897a13e3
Marty Mammel <mmammel12@gmail.com>
2024-03-15 21:16:52 -0700

    remove fterm

--- lua/config/lazy/fterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"numToStr/FTerm.nvim",
-	config = function()
-		require("FTerm").setup({})
-
-		vim.keymap.set("n", "<C-i>", '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Open FTerm" })
-		vim.keymap.set("t", "<C-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = "Close FTerm" })
-	end,
-}
